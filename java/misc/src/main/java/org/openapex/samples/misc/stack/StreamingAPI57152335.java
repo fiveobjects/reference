4fad94c0236541f74e7090cc764296fe943f9282
Five Objects <fiveobjects@gmail.com>
2019-07-23 01:57:20 +0530

    Add comments to java8 streaming example

--- java/misc/src/main/java/org/openapex/samples/misc/stack/StreamingAPI57152335.java
+++ java/misc/src/main/java/org/openapex/samples/misc/stack/StreamingAPI57152335.java
@@ -1,26 +1,42 @@
 package org.openapex.samples.misc.stack;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamingAPI57152335 {
     public static void main(String[] args) {
+        // Create attribute values
         List<AttributeValue> attributeList = createAttributes();
+        // Create empty options where the result will be populated
         List<AttributeOption> optionList = new ArrayList<AttributeOption>();
-        exampleCurrentWay(attributeList, optionList);
-        System.out.println("Attributes: "+attributeList);
-        System.out.println("Options: "+optionList);
-        optionList = new ArrayList<AttributeOption>();
-        exampleStreamingApiWay(attributeList, optionList);
-        System.out.println("Attributes: "+attributeList);
-        System.out.println("Options: "+optionList);
-    }
 
-    private static void exampleCurrentWay(List<AttributeValue> attributeList, List<AttributeOption> optionList){
-        if(attributeList != null){
-            for(AttributeValue aValue : attributeList){
-                if(aValue.getAttributeType().equalsIgnoreCase("Select")){
+        // Filter and add in traditional way
+        exampleCurrentWay(attributeList, optionList);
+        System.out.println("Attributes: " + attributeList);
+        System.out.println("Options: " + optionList);
+
+        // Resetting option list
+        optionList = new ArrayList<AttributeOption>();
+        // Filter and add in streaming api way
+        exampleStreamingApiWay(attributeList, optionList);
+        System.out.println("Attributes: " + attributeList);
+        System.out.println("Options: " + optionList);
+
+        // Resetting option list
+        optionList = new ArrayList<AttributeOption>();
+        // Streaming api way with null list as input
+        exampleStreamingApiWay(null, optionList);
+        System.out.println("Options: " + optionList);
+    }
+    // Traditional loop way
+    private static void exampleCurrentWay(List<AttributeValue> attributeList, List<AttributeOption> optionList) {
+        if (attributeList != null) {
+            for (AttributeValue aValue : attributeList) {
+                if (aValue.getAttributeType().equalsIgnoreCase("Select")) {
                     //AttributeOption aOption = service.getAttributeOption(accessToken, value.getId());
                     AttributeOption aOption = new AttributeOption(aValue.getCode());
                     aOption.setAttributeCode(aValue.getCode());
@@ -29,15 +45,13 @@ public class StreamingAPI57152335 {
             }
         }
     }
-
-    private static void exampleStreamingApiWay(List<AttributeValue> attributeList, List<AttributeOption> optionList){
-        if(attributeList != null){
-            optionList.addAll(attributeList.stream().filter(attribute -> attribute.getAttributeType().equals("Select"))
-                    .map(attribute -> new AttributeOption(attribute.getCode())).collect(Collectors.toList()));
-        }
+    // Use streaming api
+    private static void exampleStreamingApiWay(List<AttributeValue> attributeList, List<AttributeOption> optionList) {
+        optionList.addAll(Optional.ofNullable(attributeList).map(Collection::stream).orElseGet(Stream::empty).filter(attribute -> attribute.getAttributeType().equals("Select"))
+                .map(attribute -> new AttributeOption(attribute.getCode())).collect(Collectors.toList()));
     }
 
-    private static List<AttributeValue> createAttributes(){
+    private static List<AttributeValue> createAttributes() {
         List<AttributeValue> attribute = new ArrayList<AttributeValue>();
         attribute.add(new AttributeValue("Select", "001"));
         attribute.add(new AttributeValue("Abc", "002"));
@@ -46,12 +60,12 @@ public class StreamingAPI57152335 {
         attribute.add(new AttributeValue("Xyz", "005"));
         return attribute;
     }
-
-    private static  class AttributeValue {
+    // AttributeValue class
+    private static class AttributeValue {
         private String attributeType;
         private String code;
 
-        public AttributeValue(String attributeType, String code){
+        public AttributeValue(String attributeType, String code) {
             this.attributeType = attributeType;
             this.code = code;
         }
@@ -80,11 +94,11 @@ public class StreamingAPI57152335 {
                     '}';
         }
     }
-
+    // AttributeOption class
     private static class AttributeOption {
         private String attributeCode;
 
-        public AttributeOption(String attributeCode){
+        public AttributeOption(String attributeCode) {
             this.attributeCode = attributeCode;
         }
 
