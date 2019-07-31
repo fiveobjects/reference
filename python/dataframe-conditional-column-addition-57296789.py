f90ba2301925f2a47ab483be55db27ba15698a78
Five Objects <fiveobjects@gmail.com>
2019-08-01 00:16:03 +0530

    Change format of a python sample

--- python/dataframe-conditional-column-addition-57296789.py
+++ python/dataframe-conditional-column-addition-57296789.py
@@ -11,9 +11,9 @@ df.loc[pd.to_datetime(df[0]) > pd.to_datetime(df[1]), "var2"] = 0
 
 
 print(df)
-# 0           1           var
-# 0   2019-07-05  2019-06-01  1.0
-# 1   2019-07-18  2019-09-03  0.0
+#    0           1            var1  var2
+# 0  2019-07-05  2019-06-01   1.0   0.0
+# 1  2019-07-18  2019-09-03   0.0   1.0
 
 
 df['var1'] = np.where(pd.to_datetime(df[0]) > pd.to_datetime(df[1]), 1, 0)
@@ -21,6 +21,6 @@ df['var2'] = np.where(pd.to_datetime(df[0]) <= pd.to_datetime(df[1]), 1, 0)
 
 print(df)
 
-# 0           1           var
-# 0   2019-07-05  2019-06-01  1
-# 1   2019-07-18  2019-09-03  0
+#     0           1             var1  var2
+# 0  2019-07-05  2019-06-01     1     0
+# 1  2019-07-18  2019-09-03     0     1
