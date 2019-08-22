6aa3ce4cd81f79b91c56d0a7fae699847b82ff8a
Five Objects <fiveobjects@gmail.com>
2019-08-22 12:38:08 +0000

    Add manifest, agent jar for instrumentation

--- java/misc/src/main/java/org/openapex/samples/misc/instrument/ObjectSize.java
+++ java/misc/src/main/java/org/openapex/samples/misc/instrument/ObjectSize.java
@@ -1,15 +1,33 @@
 package org.openapex.samples.misc.instrument;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.io.File;
 import java.lang.instrument.Instrumentation;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ObjectSize {
     public static void main(String[] args) {
         File f = new File("data/user.csv");
         String hello = "Hello World";
         Integer num = Integer.valueOf(100);
-        System.out.println("Size of file object (bytes): "+MyInstrumentationAgent.getObjectSize(f));
-        System.out.println("Size of string (bytes): "+MyInstrumentationAgent.getObjectSize(hello));
-        System.out.println("Size of integer (bytes): "+MyInstrumentationAgent.getObjectSize(num));
+        Map<Integer, Employee> employees = new HashMap<>();
+        employees.put(1, new Employee("abc", "address1"));
+        employees.put(2, new Employee("def", "address2"));
+        employees.put(3, new Employee("ghi", "address3"));
+        employees.put(4, new Employee("jkl", "address4"));
+        System.out.println("Size of file object (bytes): " + MyInstrumentationAgent.getObjectSize(f));
+        System.out.println("Size of string (bytes): " + MyInstrumentationAgent.getObjectSize(hello));
+        System.out.println("Size of integer (bytes): " + MyInstrumentationAgent.getObjectSize(num));
+        System.out.println("Size of map (bytes): " + MyInstrumentationAgent.getObjectSize(employees));
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class Employee {
+        private String name;
+        private String address;
     }
 }
